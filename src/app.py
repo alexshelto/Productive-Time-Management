5b4c8e1a8fc7cb0f9d5ddade1ebab2c0f0bc31ec
alexshelto <alexandershelton@icloud.com>
2020-10-19 15:41:41 -0400

    oop with menu

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 
 # import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 
 
 LARGE_FONT= ("Verdana", 12)
@@ -11,33 +12,61 @@ class ProductivityApp(Tk):
   
   def __init__(self, *args, **kwargs):
     Tk.__init__(self, *args, **kwargs)
-    self.geometry("300x250")
+    Tk.wm_title(self, 'Productivity Timer') #title
+
+
     container = Frame(self)
     container.pack(side='top', fill='both', expand=True)
     container.grid_rowconfigure(0, weight=1)
     container.grid_columnconfigure(0, weight=1)
-    self.frames = {}
 
-
-    frame = SetTimePage(container, self) #initial page
-
-    self.frames[SetTimePage] = frame
-    frame.grid(row=0, column=0, sticky='nsew')
-    frame.configure(bg=PURPLE)
+    self.displayMenu(container) #displaying menu
+    
     
 
+
+
+    #rendering frames to the window 
+    self.frames = {}  #dict of pages
+    frame = SetTimePage(container, self) #initial page
+    self.frames[SetTimePage] = frame
+    frame.grid(row=0, column=0, sticky='nsew')
+  
     self.show_frame(SetTimePage)
+
+
+  def displayMenu(self, container):
+    #menu configuration
+    menuBar = Menu(container)
+    self.config(menu=menuBar)
+    #home menu
+    homeBtn = Menu(menuBar)
+    homeBtn.add_command(label='Home', command= lambda: messagebox.showinfo("Time Countdown", "Time's up "))
+    menuBar.add_cascade(label='Home', menu=homeBtn)
+    #my data menu
+    myDataBtn = Menu(menuBar)
+    myDataBtn.add_command(label='My data', command= lambda: messagebox.showinfo("Time Countdown", "Time's up "))
+    menuBar.add_cascade(label='My data', menu=myDataBtn)
+    #help menu
+    helpBtn = Menu(menuBar)
+    helpBtn.add_command(label='How this works', command= lambda: messagebox.showinfo("help page", "help popup"))
+    menuBar.add_cascade(label='Help', menu=helpBtn)
+    #exit menu 
+    exitBtn = Menu(menuBar)
+    exitBtn.add_command(label='Exit', command= lambda: messagebox.showinfo("Exit popup", "exit popup"))
+    menuBar.add_cascade(label='Help', menu=exitBtn)
 
 
   def show_frame(self, cont):
     frame = self.frames[cont]
-
     frame.tkraise()
 
 
 class SetTimePage(Frame):
   def __init__(self, parent, controller):
     Frame.__init__(self, parent)
+    self.configure(bg=PURPLE)
+
     #initialization
     label = Label(self, text='Set Productivity', font=LARGE_FONT)
     label.pack(pady=10, padx=10)
@@ -59,4 +88,5 @@ def submit():
 
 if __name__ == '__main__':
   app = ProductivityApp()
+  app.geometry("300x250")
   app.mainloop()
